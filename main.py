6edd731b774466fd3a49f726c99207f690b5c6c4
jack-dark <decker3d@gmail.com>
2026-01-13 13:44:10 -0800

    feat: adds ability for AI bot to access select files

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,25 +13,55 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    generate_content()
-
-
-def generate_content():
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    try:
+        generate_content(client, messages, args.verbose)
+
+    except Exception as e:
+        # clean top-level error reporting
+        print("===================================")
+        print("Error while calling the Gemini API.")
+        print("===================================")
+        print(e)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=messages
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+            temperature=0,
+        ),
     )
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    if not response.function_calls:
+        if verbose:
+            if response.usage_metadata:
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(
+                    f"Response tokens: {response.usage_metadata.candidates_token_count}"
+                )
+        print(response.text)
+    else:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
 
 
 if __name__ == "__main__":
